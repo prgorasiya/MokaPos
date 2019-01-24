542cc375bbe2361b46da3c7a063c7948d6de5d4c
Paras <prgorasiya@yahoo.com>
2019-01-24 14:50:35 +0800

    Initial Commit

---
+++ MokaPos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MokaPos
+//
+//  Created by paras gorasiya on 24/1/19.
+//  Copyright © 2019 paras gorasiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
